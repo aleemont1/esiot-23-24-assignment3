0e853b9ff41817ee55b539a27c9e9a1e0a99d955
Emanuele Dajko <emanuele.dajko@studio.unibo.it>
2024-01-27 16:51:15 +0100

    Remove unused code and add SerialCommunicationChannel

--- water-channel-controller/src/main.cpp
+++ water-channel-controller/src/main.cpp
@@ -4,22 +4,11 @@
 void setup()
 {
   Serial.begin(9600);
-
-  // SerialCommunicationChannel commChannel;
-
-  // // Test sending a message
-  // commChannel.sendMessage("Hello from Arduino!");
-
-  // // Test checking message availability
-  // bool isAvailable = commChannel.checkMessageAvailability();
-  // Serial.println(isAvailable ? "Message is available" : "No message available");
-
-  // // Test getting received content
-  // String content = commChannel.getReceivedContent();
-  // Serial.println("Received content: " + content);
 }
 
 void loop()
 {
-  Serial.println("Hello from Arduino!");
+
+  SerialCommunicationChannel commChannel;
+  String content = commChannel.getReceivedContent();
 }
