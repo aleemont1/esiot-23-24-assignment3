d0870520c48f2ff57b026638db327f2d13dcfe3a
Emanuele Dajko <emanuele.dajko@studio.unibo.it>
2024-02-08 20:23:43 +0100

    Add MessageReceiverImpl.cpp for serial communication

---
+++ water-channel-controller/src/serial_communication/Client-Arduino/MessageReceiverImpl.cpp
@@ -0,0 +1,46 @@
+#include "MessageReceiver.h"
+
+MessageReceiver::MessageReceiver()
+    : messageAvailable(false) {}
+
+MessageReceiver::~MessageReceiver() {}
+
+String MessageReceiver::getReceivedContent()
+{
+    unsigned long startTime = millis();
+    String receivedContent = "";
+
+    while (millis() - startTime < WAIT_TIME)
+    {
+        if (checkMessageAvailability())
+        {
+            // Read the message from the serial port
+            receivedContent = Serial.readStringUntil('\n');
+
+            // After reading, send a confirmation back to the server
+            messageSender.sentMessageConfirmation(receivedContent);
+
+            // Exit the loop once the message is received
+            break;
+        }
+    }
+
+    return receivedContent;
+}
+
+bool MessageReceiver::checkMessageAvailability()
+{
+    messageAvailable = Serial.available() > 0;
+    setMessageAvailable(true);
+    return messageAvailable;
+}
+
+void MessageReceiver::setMessageAvailable(bool messageAvailable)
+{
+    this->messageAvailable = messageAvailable;
+}
+
+bool MessageReceiver::isMessageAvailable()
+{
+    return messageAvailable;
+}
