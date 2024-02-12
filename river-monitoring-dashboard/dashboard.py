01e0ca664243e096e4e44d5be04d6619948ac487
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2024-02-13 00:53:26 +0100

    [DASHBD] Add default fonts

--- river-monitoring-dashboard/dashboard.py
+++ river-monitoring-dashboard/dashboard.py
@@ -16,7 +16,6 @@ server = Flask(__name__)
 
 # Inizializza l'app Dash con il foglio di stile esterno
 app = dash.Dash(__name__, server=server)
-app.css.append_css({"external_url": "https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"})
 # Definisci la struttura della dashboard
 app.layout = html.Div([
     html.H1("River Monitoring Dashboard", style={"text-align": "center", "font-size": "48px"}),
