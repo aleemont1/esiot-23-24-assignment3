2e8919e9151ffa0a867424552a61fd754c5acc18
Emanuele Dajko <emanuele.dajko@studio.unibo.it>
2024-02-13 19:09:09 +0100

    Add Logger and ServoMotor dependencies to SerialCommunicationTask

--- water-channel-controller/src/task/SerialCommunicationTask.h
+++ water-channel-controller/src/task/SerialCommunicationTask.h
@@ -6,6 +6,8 @@
 #include "serial_communication/Client-Arduino/JsonProcessor.h"
 #include "serial_communication/Client-Arduino/MessageReceiver.h"
 #include "serial_communication/Client-Arduino/MessageSender.h"
+#include "system/Logger.h"
+#include "../components/api/ServoMotor.h"
 #include "Arduino.h"
 
 class SerialCommunicationTask : public Task
@@ -14,20 +16,24 @@ public:
     SerialCommunicationTask(int period, JsonProcessor jsonProcessor, WaterChannelController *waterChannelController);
     void initializeSerialCommunication();
     void receivedEndMessage();
+    String formatMessage(String status, String valveValue);
+    String createConfirmationMessage(String originalMessage);
+    String getSystemStatus(String receivedMessage);
     void tick() override;
 
 private:
+    unsigned long BAUD_RATE = 9600;
     WaterChannelController *waterChannelController;
     unsigned long int lastMessage{0};
     MessageReceiver messageReceiver;
     MessageSender messageSender;
     JsonProcessor jsonProcessor;
-    const int BAUD_RATE = 9600;
     String status;
     String valveValue;
     String content;
     bool messageAvailable;
     bool messageDelivered;
+    ServoMotor *motor; ///< The ServoMotor used to control the valve.
 };
 
 #endif // __SERIAL_COMMUNICATION_H__
