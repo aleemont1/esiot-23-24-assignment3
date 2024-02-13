0d95a58f9db0ed939819b49ad0dbb92177d7660a
Emanuele Dajko <emanuele.dajko@studio.unibo.it>
2024-02-13 22:31:55 +0100

    Update Config.h with constant configuration values

--- water-channel-controller/src/Config.h
+++ water-channel-controller/src/Config.h
@@ -1,14 +1,42 @@
-#ifndef __CONFIG__
-#define __CONFIG__
-#define __DEBUG__
+#ifndef CONFIG_H
+#define CONFIG_H
 
 #include "Arduino.h"
 
+/**
+ * @brief Configuration file for the system.
+ *
+ * This file contains all the constant configuration values used throughout the system.
+ */
+
+/**
+ * @brief The base period for tasks in milliseconds.
+ */
 constexpr int BASE_PERIOD = 100;
+
+/**
+ * @brief The analog pin connected to the potentiometer.
+ */
 constexpr int POT_PIN = A0;
+
+/**
+ * @brief The digital pin connected to the button.
+ */
 constexpr int BUTTON_PIN = 4;
+
+/**
+ * @brief The timeout for the connection in milliseconds.
+ */
 constexpr long TIME_OUT_CONNECTION = 300000L;
+
+/**
+ * @brief The digital pin connected to the servo motor.
+ */
 constexpr int SERVO_PIN = 9;
+
+/**
+ * @brief The time to move the servo motor in milliseconds.
+ */
 constexpr int TIME_TO_MOVE = 20;
 
-#endif // __CONFIG__
+#endif // CONFIG_H
