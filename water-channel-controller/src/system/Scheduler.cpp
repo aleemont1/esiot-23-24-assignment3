2122011a0e0ae5096281147b5e67905cd6d7bf78
Emanuele Dajko <emanuele.dajko@studio.unibo.it>
2024-02-13 22:33:37 +0100

    Fix capitalization and include statements in Scheduler.cpp

--- water-channel-controller/src/system/Scheduler.cpp
+++ water-channel-controller/src/system/Scheduler.cpp
@@ -1,51 +1,53 @@
-#include "scheduler.h"
+#include "Scheduler.h"
 #include "TimerOne.h"
-#include "logger.h"
+#include "Logger.h"
 
-volatile bool timerFlag;
+// A flag to indicate if the timer has run.
+volatile bool timerFlag = false;
 
 void timerHandler(void)
 {
-    if (timerFlag == true)
+    if (timerFlag)
     {
         // logger("Scheduler: Timer overrun\n");
+        return;
     }
     timerFlag = true;
 }
 
 Scheduler::Scheduler(int basePeriod) : basePeriod(basePeriod)
 {
-    timerFlag = false;
-    long period = 1000l * basePeriod;
+    // The conversion is preventing potential overflow if basePeriod is a large int.
+    const long period = 1000l * static_cast<long>(basePeriod);
     Timer1.initialize(period);
     Timer1.attachInterrupt(timerHandler);
 }
 
 bool Scheduler::addTask(Task *task)
 {
-    if (nTasks < MAX_TASKS - 1)
-    {
-        taskList[nTasks] = task;
-        nTasks++;
-        return true;
-    }
-    else
+    if (task == nullptr || nTasks >= MAX_TASKS)
     {
         return false;
     }
+
+    taskList[nTasks++] = task;
+    return true;
 }
 
 void Scheduler::schedule()
 {
     while (!timerFlag)
     {
+        // Do nothing, just wait for the timerFlag to be set
     }
-    for (int i = 0; i < nTasks; i++)
+
+    for (Task *task : taskList)
     {
-        if (taskList[i]->updateAndCheckTime(basePeriod))
+        if (task && task->updateAndCheckTime(basePeriod))
         {
-            taskList[i]->tick();
+            task->tick();
         }
     }
+
     timerFlag = false;
 }
