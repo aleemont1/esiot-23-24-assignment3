433d905ba9c6af5bd7ae54ed6f2609475fca9b85
Emanuele Dajko <emanuele.dajko@studio.unibo.it>
2024-02-13 22:34:05 +0100

    Refactor LcdTask class to improve readability and maintainability

--- water-channel-controller/src/task/LcdTask.cpp
+++ water-channel-controller/src/task/LcdTask.cpp
@@ -1,18 +1,39 @@
 #include "LcdTask.h"
 
-#include "components/api/LCD.h"
-
-LcdTask::LcdTask(int period, WaterChannelController *waterChannelController, uint8_t lcd_Addr, uint8_t lcd_cols, uint8_t lcd_rows)
-    : Task(period), waterChannelController(waterChannelController), lcd(new Lcd(lcd_Addr, lcd_cols, lcd_rows)), colDisplay(lcd_cols)
+LcdTask::LcdTask(int period, WaterChannelController *waterChannelController, uint8_t lcdAddr, uint8_t lcdCols, uint8_t lcdRows)
+    : Task(period),
+      waterChannelController(waterChannelController),
+      lcd(new Lcd(lcdAddr, lcdCols, lcdRows)),
+      colDisplay(lcdCols)
 {
-    this->lcd->clear();
-    this->lcd->show("Water Channel Controller");
+    initializeLcd();
 }
 
 void LcdTask::tick()
 {
-    const String mode = waterChannelController->activeMode == Mode::AUTOMATIC ? "Automatic: " : "Manual: ";
-    const String position = String(waterChannelController->activePosition);
+    displayModeAndPosition();
+}
+
+void LcdTask::initializeLcd()
+{
+    lcd->clear();
+    lcd->show("Water Channel Controller");
+}
+
+void LcdTask::displayModeAndPosition()
+{
+    const String mode = getModeString();
+    const String position = getPositionString();
     String message = mode + position;
     lcd->show(message);
+}
+
+String LcdTask::getModeString()
+{
+    return (waterChannelController->activeMode == Mode::AUTOMATIC) ? "Automatic: " : "Manual: ";
+}
+
+String LcdTask::getPositionString()
+{
+    return String(waterChannelController->activePosition);
 }
