d68d2607b76ab51a32c5b056f0decb85266fd687
Emanuele Dajko <emanuele.dajko@studio.unibo.it>
2024-02-13 16:04:31 +0100

    Update user authentication logic

--- water-channel-controller/src/serial_communication/Client-Arduino/SerialCommunication.h
+++ water-channel-controller/src/serial_communication/Client-Arduino/SerialCommunication.h
@@ -1,120 +1,128 @@
-#ifndef __COMMUNICATION_H__
-#define __COMMUNICATION_H__
+// #ifndef __COMMUNICATION_H__
+// #define __COMMUNICATION_H__
 
-#include "Arduino.h"
-#include "ArduinoJson.h"
-#include "JsonProcessor.h"
-#include "MessageSender.h"
-#include "MessageReceiver.h"
+// #include "Arduino.h"
+// #include "ArduinoJson.h"
+// #include "JsonProcessor.h"
+// #include "MessageSender.h"
+// #include "MessageReceiver.h"
+// #include "system/Logger.h"
+// #include "Config.h"
+// #include "system/Task.h"
+// #include "WaterChannelController.h"
 
-/**
- * @brief Manages a serial communication channel between a client and a server.
- *
- * The SerialCommunicationChannel class provides a high-level interface
- * for managing serial communication between a client (an Arduino board)
- * and a server (a computer).
- * It encapsulates the details of sending and receiving messages over a serial port,
- * and provides methods for processing received messages, checking message availability,
- * and closing the communication channel.
- *
- * Key responsibilities of this class include:
- * - Processing incoming messages and formatting outgoing messages.
- * - Managing the state of the communication channel (open, closed).
- * - Checking the availability of incoming messages.
- *
- * @note This class is specifically designed for communication where
- * the client is an Arduino board and the server is a computer.
- */
-class SerialCommunicationChannel
-{
+// /**
+//  * @brief Manages a serial communication channel between a client and a server.
+//  *
+//  * The SerialCommunicationChannel class provides a high-level interface
+//  * for managing serial communication between a client (an Arduino board)
+//  * and a server (a computer).
+//  * It encapsulates the details of sending and receiving messages over a serial port,
+//  * and provides methods for processing received messages, checking message availability,
+//  * and closing the communication channel.
+//  *
+//  * Key responsibilities of this class include:
+//  * - Processing incoming messages and formatting outgoing messages.
+//  * - Managing the state of the communication channel (open, closed).
+//  * - Checking the availability of incoming messages.
+//  *
+//  * @note This class is specifically designed for communication where
+//  * the client is an Arduino board and the server is a computer.
+//  */
+// class SerialCommunicationChannel : public Task
+// {
 
-public:
-    /**
-     * @brief Constructs a new SerialCommunicationChannel object.
-     */
-    SerialCommunicationChannel(JsonProcessor jsonProcessor);
+// public:
+//     /**
+//      * @brief Constructs a new SerialCommunicationChannel object.
+//      */
+//     SerialCommunicationChannel(JsonProcessor jsonProcessor, int period, WaterChannelController *waterChannelController);
 
-    /**
-     * @brief Uses RAII to automatically close the serial port when the object is destroyed.
-     */
-    ~SerialCommunicationChannel();
+//     void tick() override;
 
-    /**
-     * @brief Initializes the serial communication channel.
-     *
-     * This function sets up the serial communication with the Arduino board.
-     * It should be called before any other serial communication functions are used.
-     */
-    void initializeSerialCommunication();
+//     /**
+//      * @brief Uses RAII to automatically close the serial port when the object is destroyed.
+//      */
+//     ~SerialCommunicationChannel();
 
-private:
-    /**
-     * @brief An instance of the JsonProcessor class.
-     *
-     * This instance, jsonProcessor, is used to handle JSON-formatted messages within the SerialCommunication class. It provides methods for processing these messages, including parsing the JSON content, extracting relevant information, and formatting it for use by other parts of the application.
-     */
-    JsonProcessor jsonProcessor;
+//     /**
+//      * @brief Initializes the serial communication channel.
+//      *
+//      * This function sets up the serial communication with the Arduino board.
+//      * It should be called before any other serial communication functions are used.
+//      */
+//     void initializeSerialCommunication();
 
-    /**
-     * @brief The status of the valve received from the server.
-     *
-     * The valve's status depends on the received message.
-     */
-    String status;
+// private:
+//     /**
+//      * @brief An instance of the JsonProcessor class.
+//      *
+//      * This instance, jsonProcessor, is used to handle JSON-formatted messages within the SerialCommunication class. It provides methods for processing these messages, including parsing the JSON content, extracting relevant information, and formatting it for use by other parts of the application.
+//      */
+//     JsonProcessor jsonProcessor;
 
-    /**
-     * @brief The valve value received from the server.
-     *
-     * This value is used to control the valve's opening and closing percentage.
-     */
-    String valveValue;
+//     /**
+//      * @brief The status of the valve received from the server.
+//      *
+//      * The valve's status depends on the received message.
+//      */
+//     String status;
 
-    /**
-     * @brief The content of the message received from the server.
-     */
-    String content;
+//     /**
+//      * @brief The valve value received from the server.
+//      *
+//      * This value is used to control the valve's opening and closing percentage.
+//      */
+//     String valveValue;
 
-    /**
-     * @brief The baud rate used by the serial communication channel.
-     */
-    const int BAUD_RATE = 9600;
+//     /**
+//      * @brief The content of the message received from the server.
+//      */
+//     String content;
 
-    /**
-     * @brief The maximum amount of time to wait for data to be available on the serial port.
-     *
-     * This constant defines the maximum amount of time, in milliseconds, that the system
-     * will wait for data to become available on the serial port before timing out.
-     * It is used in the getReceivedContent method to control how long the method waits for incoming data.
-     */
-    const unsigned long WAIT_TIME = 500;
+//     /**
+//      * @brief The baud rate used by the serial communication channel.
+//      */
+//     const int BAUD_RATE = 9600;
 
-    /**
-     * @brief A flag indicating whether a message is available
-     * on the serial port to be read by the client.
-     */
-    bool messageAvailable;
+//     /**
+//      * @brief The maximum amount of time to wait for data to be available on the serial port.
+//      *
+//      * This constant defines the maximum amount of time, in milliseconds, that the system
+//      * will wait for data to become available on the serial port before timing out.
+//      * It is used in the getReceivedContent method to control how long the method waits for incoming data.
+//      */
+//     const unsigned long WAIT_TIME = 500;
 
-    /**
-     * @brief A flag indicating whether my message has been
-     * delivered to the server.
-     */
-    bool messageDelivered;
+//     /**
+//      * @brief A flag indicating whether a message is available
+//      * on the serial port to be read by the client.
+//      */
+//     bool messageAvailable;
 
-    /**
-     * @brief Processes a specific end message received from the server
-     * to close the communication channel.
-     */
-    void receivedEndMessage();
+//     /**
+//      * @brief A flag indicating whether my message has been
+//      * delivered to the server.
+//      */
+//     bool messageDelivered;
 
-    /**
-     * @brief an instance of the MessageSender class.
-     */
-    MessageSender messageSender;
+//     /**
+//      * @brief Processes a specific end message received from the server
+//      * to close the communication channel.
+//      */
+//     void receivedEndMessage();
 
-    /**
-     * @brief an instance of the MessageReceiver class.
-     */
-    MessageReceiver messageReceiver;
-};
+//     /**
+//      * @brief an instance of the MessageSender class.
+//      */
+//     MessageSender messageSender;
 
-#endif // __COMMUNICATION_H__
+//     /**
+//      * @brief an instance of the MessageReceiver class.
+//      */
+//     MessageReceiver messageReceiver;
+
+//     WaterChannelController *waterChannelController;
+// };
+
+// #endif // __COMMUNICATION_H__
