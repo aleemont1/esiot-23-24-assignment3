cd3ef5f457558822b741b0e704b7a9a4112046a6
Emanuele Dajko <emanuele.dajko@studio.unibo.it>
2024-02-13 19:08:47 +0100

    Add Serial.begin(9600) to setup() function

--- water-channel-controller/src/main.cpp
+++ water-channel-controller/src/main.cpp
@@ -12,13 +12,14 @@ WaterChannelController waterChannelController;
 
 void setup()
 {
+  Serial.begin(9600);
   scheduler = new Scheduler(BASE_PERIOD);
 
   // Initialize the task and add them to the scheduler
   scheduler->addTask(new LcdTask(150, &waterChannelController, 0x27, 16, 2));
   scheduler->addTask(new InputTask(100, &waterChannelController, BUTTON_PIN, POT_PIN));
   scheduler->addTask(new ValveTask(105, &waterChannelController, SERVO_PIN));
-  scheduler->addTask(new SerialCommunicationTask(100, JsonProcessor(), &waterChannelController));
+  scheduler->addTask(new SerialCommunicationTask(200, JsonProcessor(), &waterChannelController));
 }
 
 void loop()
