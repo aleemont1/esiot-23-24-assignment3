93b76b8f3adafbc271202d55d042e44fdc789114
Emanuele Dajko <emanuele.dajko@studio.unibo.it>
2024-02-14 23:57:14 +0100

    Add serialComm() function to handle serial communication

--- water-channel-controller/src/task/SerialCommunicationTask.cpp
+++ water-channel-controller/src/task/SerialCommunicationTask.cpp
@@ -15,8 +15,58 @@ SerialCommunicationTask::SerialCommunicationTask(int period, JsonProcessor jsonP
     Serial.begin(BAUD_RATE);
 }
 
+void SerialCommunicationTask::serialComm()
+{
+    // ServoMotor *servoMotor;
+    String receivedContent = "";
+    JsonDocument doc;
+    deserializeJson(doc, receivedContent);
+    String status = doc["status"];
+    String valveValue;
+    if (Serial.available() > 0)
+    {
+        receivedContent = Serial.readStringUntil('\n');
+
+        if (status == "ALARM_TOO_LOW")
+        {
+            valveValue = "0";
+        }
+        else if (status == "NORMAL")
+        {
+            valveValue = "25";
+        }
+        else if (status == "PRE_ALARM_TOO_HIGH")
+        {
+            valveValue = "40";
+        }
+        else if (status == "ALARM_TOO_HIGH")
+        {
+            valveValue = "50";
+        }
+        else if (status == "ALARM_TOO_HIGH_CRITIC")
+        {
+            valveValue = "100";
+        }
+        else
+        {
+            valveValue = "unknown state";
+        }
+
+        // Creazione del documento JSON
+        JsonDocument jsonDoc;
+        jsonDoc["status"] = status;
+        jsonDoc["valveValue"] = valveValue;
+
+        // Serializzazione del documento JSON
+        String output;
+        serializeJson(jsonDoc, output);
+
+        // Stampa il documento JSON sulla seriale
+        Serial.println(output);
+    }
+}
+
 void SerialCommunicationTask::tick()
 {
-    messageReceiver.checkMessageAvailability();
-    messageReceiver.getReceivedContent();
+    serialComm();
 }
