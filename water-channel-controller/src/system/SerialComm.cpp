7470071366e81bfc9318739db6d00118b1d48c7d
Emanuele Dajko <emanuele.dajko@studio.unibo.it>
2024-02-15 02:54:36 +0100

    Refactor SerialComm class to improve response creation and serial communication

--- water-channel-controller/src/system/SerialComm.cpp
+++ water-channel-controller/src/system/SerialComm.cpp
@@ -1,39 +1,43 @@
 #include "SerialComm.h"
 
+String SerialComm::createResponse(String status)
+{
+    if (status == "ALARM_TOO_LOW")
+    {
+        return "{\"status\": \"ALARM_TOO_LOW\",\"valveValue\": 0}";
+    }
+    else if (status == "NORMAL")
+    {
+        return "{\"status\":\"NORMAL\",\"valveValue\": 25}";
+    }
+    else if (status == "PRE_ALARM_TOO_HIGH")
+    {
+        return "{\"status\":\"PRE_ALARM_TOO_HIGH\",\"valveValue\": 40}";
+    }
+    else if (status == "ALARM_TOO_HIGH")
+    {
+        return "{\"status\":\"ALARM_TOO_HIGH\",\"valveValue\": 50}";
+    }
+    else if (status == "ALARM_TOO_HIGH_CRITIC")
+    {
+        return "{\"status\":\"ALARM_TOO_HIGH_CRITIC\",\"valveValue\": 100}";
+    }
+    else
+    {
+        return "unknown state";
+    }
+}
+
 void SerialComm::serialCommunication()
 {
     String receivedContent;
-    String valveValue;
     if (Serial.available())
     {
         receivedContent = Serial.readStringUntil('\n');
         JsonDocument doc;
         deserializeJson(doc, receivedContent);
         String status = doc["status"];
-
-        if (status == "ALARM_TOO_LOW")
-        {
-            Serial.println("{\"status\": \"ALARM_TOO_LOW\",\"valveValue\": 0}");
-        }
-        else if (status == "NORMAL")
-        {
-            Serial.println("{\"status\":\"NORMAL\",\"valveValue\": 25}");
-        }
-        else if (status == "PRE_ALARM_TOO_HIGH")
-        {
-            Serial.println("{\"status\":\"PRE_ALARM_TOO_HIGH\",\"valveValue\": 40}");
-        }
-        else if (status == "ALARM_TOO_HIGH")
-        {
-            Serial.println("{\"status\":\"ALARM_TOO_HIGH\",\"valveValue\": 50}");
-        }
-        else if (status == "ALARM_TOO_HIGH_CRITIC")
-        {
-            Serial.println("{\"status\":\"ALARM_TOO_HIGH_CRITIC\",\"valveValue\": 100}");
-        }
-        else
-        {
-            valveValue = "unknown state";
-        }
+        String response = createResponse(status);
+        Serial.println(response);
     }
 }
